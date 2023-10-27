be07c4d7c559d7db6a06fe62b372afc25cf44575
ereekaur <113838261+ereekaur@users.noreply.github.com>
2023-10-27 09:11:58 +0300

    Update MatrixPuzzle.py

--- MatrixPuzzle.py
+++ MatrixPuzzle.py
@@ -1,16 +1,20 @@
 import pygame
 import random
 
-## ----------------
-## MATRIX PUZZLE
-## (c) Erno Kauranen
-## ---------------
+"""
+File: MatrixPuzzle.py
+Author: Erno Kauranen
+Date: 2023-10-27
+Description: Quiz game
+
+"""
 
 pygame.init()
 WIDTH, HEIGHT = 1000, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Matrix puzzle")
 logics = []
+start_time = pygame.time.get_ticks()
 
 def generate_binary_matrix_with_red_square(position):
     matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
@@ -18,6 +22,7 @@ def generate_binary_matrix_with_red_square(position):
     return matrix
 
 def difficult(position):
+   
     matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     if position == 0:
         matrix = [[1, 0, 0, 1], [0, 0, 0, 0], [1, 0, 0, 1], [0, 1, 1, 0]]
@@ -29,8 +34,8 @@ def difficult(position):
         matrix = [[1, 0, 0, 1], [0, 0, 0, 0], [0, 1, 1, 0], [1, 0, 0, 1]]
     return matrix
 
-
 def morediff(position):
+    
     matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     if position == 0:
         matrix = [[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
@@ -43,13 +48,13 @@ def morediff(position):
     return matrix
 
 def display_question_mark(x, y, size=50, color=(0, 0, 255)):
+    
     pygame.draw.rect(WINDOW, color, (x, y, size, size))
     font = pygame.font.Font(None, 100)
     text = font.render("?", True, (255, 255, 255))
     text2 = font.render("->", True, (255, 155, 0))
     text3 = font.render("->", True, (255, 155, 0))
     text4 = font.render("->", True, (255, 155, 0))
-
     text_rect = text.get_rect(center=(x + size / 2, y + size / 2))
     text_rect2 = text.get_rect(center=(500,170))
     text_rect3 = text.get_rect(center=(250,170))
@@ -59,7 +64,7 @@ def display_question_mark(x, y, size=50, color=(0, 0, 255)):
     WINDOW.blit(text3, text_rect3)
     WINDOW.blit(text4, text_rect4)
 
-def moving_red_square(position):
+def movingsquare(position):
     matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
     if position == 0:
@@ -86,13 +91,13 @@ def randomsquare(monesko,tmp,question_number):
         return logics[2][question_number]
     
     if monesko == 3:
-        return [[1, 1, 1, 1], [1, 0, 0, 1], [1, 0, 0, 1], [1, 1, 0, 1]]
-    
+        return logics[3][question_number]
+       
     
     return matrix
 
 
-# --- Questions and the answer is stored to list --------------------
+# Questions and the answer is stored in list
 
 
 question1 =        [generate_binary_matrix_with_red_square(0),
@@ -101,17 +106,17 @@ question1 =        [generate_binary_matrix_with_red_square(0),
                    generate_binary_matrix_with_red_square(3)]
 
 
-question2 = [      moving_red_square(0),
-                   moving_red_square(1),
-                   moving_red_square(2),
-                   moving_red_square(3)]
+question2 =        [movingsquare(0),
+                   movingsquare(1),
+                   movingsquare(2),
+                   movingsquare(3)]
 
-question4 =         [difficult(0),
+question4 =        [difficult(0),
                    difficult(1),
                    difficult(2),
                    difficult(3)]
 
-question3 =        [morediff(0),
+question3 =       [morediff(0),
                    morediff(1),
                    morediff(2),
                    morediff(3)]
@@ -124,9 +129,9 @@ logics.append(question2)
 logics.append(question3)
 logics.append(question4)
 
-#------------------------------------------
+#--------------------------------------------
 
-def display_matrix(matrix, x, y, square_size=50, grid_color=(0, 0, 0)):
+def display_matrix(matrix, x, y, square_size = 50, grid_color=(0, 0, 0)):
     for i in range(4):
         for j in range(4):
             color = (255, 0, 0) if matrix[i][j] == 1 else (255, 255, 255) 
@@ -134,42 +139,47 @@ def display_matrix(matrix, x, y, square_size=50, grid_color=(0, 0, 0)):
             pygame.draw.rect(WINDOW, grid_color, (x + j * square_size, y + i * square_size, square_size, square_size), 1)
 
 def create_button(x, y, width, height, color, text, text_color, font_size, action=None):
-    font = pygame.font.Font(None, font_size)
-    text_surface = font.render(text, True, text_color)
-    text_rect = text_surface.get_rect()
-    text_rect.center = (x + width / 2, y + height / 2)
-    pygame.draw.rect(WINDOW, color, (x, y, width, height))
-    WINDOW.blit(text_surface, text_rect)
-    return (x, y, width, height, action)
+
+    return (x, y, width, height, action, color, text)
 
 def check_answer(selected_option, correct_option):
+    
     return selected_option == correct_option
+
+def format_time(milliseconds):
+    total_seconds = milliseconds // 1000
+    minutes = total_seconds // 60
+    seconds = total_seconds % 60
+    return f"Time: {minutes:02d}:{seconds:02d}"
 
 running = True
 score = 0
-
 buttons = []
 
 for question_number in range(4):
     
     options = [logics[question_number][3]]
-    tmp= logics[question_number][3]
+    tmp = logics[question_number][3]
     options += [randomsquare(question_number, tmp,_) for _ in range(2)]
 
-   
     random.shuffle(options)
-    
     
     correct_option = options.index(logics[question_number][3])
 
-
-    buttons.append(create_button(160, 600, 150, 50, (0, 255, 0), "Option 1", (0, 0, 0), 24, action=0))
-    buttons.append(create_button(405, 600, 150, 50, (0, 255, 0), "Option 2", (0, 0, 0), 24, action=1))
-    buttons.append(create_button(650, 600, 150, 50, (0, 255, 0), "Option 3", (0, 0, 0), 24, action=2))
+    buttons.append(create_button(155, 600, 150, 50, (0, 255, 0), "Option 1", (255, 0, 0), 24, action=0))
+    buttons.append(create_button(405, 600, 150, 50, (0, 255, 0), "Option 2", (255, 0, 0), 24, action=1))
+    buttons.append(create_button(655, 600, 150, 50, (0, 255, 0), "Option 3", (255, 0, 0), 24, action=2))
 
     question_answered = False
-
+    time_limit = 120000
+    elapsed_time = pygame.time.get_ticks() - start_time
+    
+    remaining_time = max(time_limit - elapsed_time, 0)
+    if elapsed_time >= time_limit:
+            running =  question_answered  
+            
     while not question_answered and running:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -177,15 +187,25 @@ for question_number in range(4):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 for button in buttons:
-                    x, y, width, height, action = button
+                    x, y, width, height, action, color, text= button
                     if x <= mouse_x <= x + width and y <= mouse_y <= y + height:
                         if check_answer(action, correct_option):
                             score += 1
                         question_answered = True
 
         WINDOW.fill((0, 0, 0))  
-        
-        
+
+        elapsed_time = pygame.time.get_ticks() - start_time
+        time_limit = 120000
+        remaining_time = max(time_limit - elapsed_time, 0)
+        WINDOW.fill((0, 0, 0))
+        font = pygame.font.Font(None, 36)
+        timer_text = font.render(format_time(remaining_time), True, (255, 255, 255))
+        timer_rect = timer_text.get_rect()
+        timer_rect.topleft = (10, 10)
+        WINDOW.blit(timer_text, timer_rect)
+        if remaining_time <= 0:
+            running = False
         for i in range(3):
             display_matrix(logics[question_number][i], 50 + i * 250, 100, square_size=40)
 
@@ -197,34 +217,36 @@ for question_number in range(4):
 
         # Display the buttons
         for button in buttons:
-            x, y, width, height, _ = button
-            pygame.draw.rect(WINDOW, (0, 255, 0), (x, y, width, height))
-
-       
+            x, y, width, height, action, color, text = button
+            pygame.draw.rect(WINDOW, (0, 0, 255), (x, y, width, height))
+            text_surface = font.render(text, True, (255, 255, 0))
+            text_rect = text_surface.get_rect()
+            text_rect.center = (x + width / 2, y + height / 2)  
+            WINDOW.blit(text_surface, text_rect)
+            
         display_question_mark(50 + (i + 1) * 250, 100, size=150, color=(0, 0, 255))
 
         pygame.display.update()
 
     buttons.clear()
 
-
 def display_iq_score(score, x, y, font_size=136, color=(255, 255, 255)):
     font = pygame.font.Font(None, font_size)
-    text = "Score: "
+    text = "Your IQ is:  "
     if score == 0:
-        text += "50 IQ"
+        text += "50"
     elif score == 1:
-        text += "80 IQ"
+        text += "80"
     elif score == 2:
-        text += "100 IQ"
-    elif score == 3:
-        text += "150 IQ"
+        text += "100"
+    else:
+        text += "150"
+
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
     text_rect.topleft = (x, y)
-    WINDOW.blit(text_surface, text_rect)
 
-# points display
+    WINDOW.blit(text_surface, text_rect)
 
 running_iq_screen = True
 
