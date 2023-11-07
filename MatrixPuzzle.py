155e883b282f654bc6960eabbadc1f3e4ed6cb6d
ereekaur <113838261+ereekaur@users.noreply.github.com>
2023-11-07 15:00:45 +0200

    Update MatrixPuzzle.py

--- MatrixPuzzle.py
+++ MatrixPuzzle.py
@@ -4,19 +4,78 @@ import random
 """
 File: MatrixPuzzle.py
 Author: Erno Kauranen
-Date: 2023-10-27
+Date: 2023-11-07
 Description: Quiz game
 
 """
-
+temp_variable = 1
+temp_variable2 = 1
+temp_variable3 = 1
+temp_variable4 = 1
+temp_variable6 = 1
 pygame.init()
+
+WIDTH, HEIGHT = 1000, 800
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Matrix Puzzle")
+
+font = pygame.font.Font(None, 36)
+input_text = ''
+input_rect = pygame.Rect(300, 300, 340, 32)
+color_active = pygame.Color('lightskyblue3')
+color_passive = pygame.Color('gray15')
+color = color_passive
+active = False
+player_name = None
+name_entered = False
+
+
+while not name_entered:
+   
+    text1 = "To start the game click the field below, type your name and press Enter"
+    text2 = "WARNING! The game starts immediately!"
+
+    text_surface1 = font.render(text1, True, (255,0,0)) 
+    text_surface2 = font.render(text2, True, (255,0,0))   
+
+    window.blit(text_surface1, (120, 150))
+    window.blit(text_surface2, (220, 450))
+ 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(event.pos):
+                active = not active
+            else:
+                active = False
+            color = color_active if active else color_passive
+
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    player_name = input_text
+                    input_text = ''
+                    name_entered = True
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+                else:
+                    input_text += event.unicode
+
+  
+    pygame.draw.rect(window, color, input_rect)
+    text_surface = font.render(input_text, True, (0, 0, 0))
+    window.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+    pygame.display.flip()
+
+
 WIDTH, HEIGHT = 1000, 800
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Matrix puzzle")
 logics = []
 start_time = pygame.time.get_ticks()
 
-def generate_binary_matrix_with_red_square(position):
+def grate_bnry_matrix_with_red_sq(position):
     matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     matrix[0][position] = 1
     return matrix
@@ -47,6 +106,32 @@ def morediff(position):
         matrix = [[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1 ,1]]
     return matrix
 
+def moredifff(position):
+    
+    matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    if position == 0:
+        matrix = [[1, 1, 1, 1], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    elif position == 1:
+        matrix =[[1, 0, 0, 0], [0, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]]
+    elif position == 2:
+        matrix = [[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 1, 1], [0, 0, 0, 0]]
+    elif position == 3:
+        matrix = [[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 1]]
+    return matrix
+
+def morediffff(position):
+    
+    matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    if position == 0:
+        matrix = [[1, 0, 1, 0], [1, 1, 1, 1], [1, 0, 0, 1], [1, 0, 0, 1]]
+    elif position == 1:
+        matrix =[[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0], [1, 0, 0, 1]]
+    elif position == 2:
+        matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0]]
+    elif position == 3:
+        matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 1], [0, 1, 1, 0]]
+    return matrix
+
 def display_question_mark(x, y, size=50, color=(0, 0, 255)):
     
     pygame.draw.rect(WINDOW, color, (x, y, size, size))
@@ -64,7 +149,7 @@ def display_question_mark(x, y, size=50, color=(0, 0, 255)):
     WINDOW.blit(text3, text_rect3)
     WINDOW.blit(text4, text_rect4)
 
-def movingsquare(position):
+def movingsq(position):
     matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
     if position == 0:
@@ -75,6 +160,34 @@ def movingsquare(position):
         matrix[3][3] = 1  
     elif position == 3:
         matrix[3][0] = 1  
+    return matrix
+
+def mirror(position):
+    matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+    if position == 0:
+        matrix =[[0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]] 
+    elif position == 1:
+        matrix =[[0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 1, 0], [0, 0, 1, 0]]
+    elif position == 2:
+        matrix =[[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 1], [0, 1, 0, 0]]
+    elif position == 3:
+        matrix =[[0, 1, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0]]
+    return matrix
+
+def diagonal(position):
+    
+    matrix = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+    if position == 0:
+        matrix =[[0, 1, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]] 
+    elif position == 1:
+        matrix =[[0, 0, 0, 0], [1, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    elif position == 2:
+        matrix =[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+    elif position == 3:
+        matrix =[[0, 1, 0, 0], [0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]]
+
     return matrix
 
 def randomsquare(monesko,tmp,question_number):
@@ -88,48 +201,84 @@ def randomsquare(monesko,tmp,question_number):
    
     if monesko == 2:
 
-        return logics[2][question_number]
+        global temp_variable3
+        for i in range(2):
+                
+                if temp_variable3 == 1:
+                
+                    temp_variable3 +=1
+                    return [[1, 1, 1, 1], [1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]]
+                    
+                else: 
+                
+                    return [[0, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0]]
     
     if monesko == 3:
-        return logics[3][question_number]
+        global temp_variable4
+        for i in range(2):
+             
+             if temp_variable4 == 1:
+             
+                 temp_variable4 +=1
+                 return [[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0]]
+                 
+             else: 
+              
+              matrix =[[0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]]
+        
        
+    if monesko == 4:
+            global temp_variable
+            for i in range(2):
+             
+             if temp_variable == 1:
+             
+                 temp_variable +=1
+                 return [[1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]
+                 
+             else: 
+              
+              return [[0, 0, 0,0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 1]]
     
+    if monesko == 5:
+            global temp_variable2
+            for i in range(2):
+             
+             if temp_variable2 == 1:
+             
+                 temp_variable2 +=1
+                 return [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+                 
+             else: 
+              
+              return [[0, 0, 0,0], [0, 0, 0, 0], [1, 0, 0, 1], [1, 0, 0, 1]]
+             
+
+    if monesko == 6:
+        return logics[monesko][question_number]
+    
+    if monesko == 7:
+        global temp_variable6
+        
+        for i in range(2):
+             
+             if temp_variable6 == 1:
+             
+                 temp_variable6 +=1
+                 return [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+                 
+             else: 
+              
+              return [[0, 0, 0,0 ], [0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 1, 0]]   
     return matrix
 
 
-# Questions and the answer is stored in list
+logics = []
+functions = [grate_bnry_matrix_with_red_sq, movingsq, morediff,mirror, moredifff, morediffff,difficult,diagonal]
 
-
-question1 =        [generate_binary_matrix_with_red_square(0),
-                   generate_binary_matrix_with_red_square(1),
-                   generate_binary_matrix_with_red_square(2),
-                   generate_binary_matrix_with_red_square(3)]
-
-
-question2 =        [movingsquare(0),
-                   movingsquare(1),
-                   movingsquare(2),
-                   movingsquare(3)]
-
-question4 =        [difficult(0),
-                   difficult(1),
-                   difficult(2),
-                   difficult(3)]
-
-question3 =       [morediff(0),
-                   morediff(1),
-                   morediff(2),
-                   morediff(3)]
-
-
-# ------ Add questions to list --------------
-
-logics.append(question1)
-logics.append(question2)
-logics.append(question3)
-logics.append(question4)
-
-#--------------------------------------------
+for func in functions:
+    questions = [func(i) for i in range(4)]
+    logics.append(questions)
 
 def display_matrix(matrix, x, y, square_size = 50, grid_color=(0, 0, 0)):
     for i in range(4):
@@ -156,7 +305,8 @@ running = True
 score = 0
 buttons = []
 
-for question_number in range(4):
+for question_number in range(len(logics)):
+
     
     options = [logics[question_number][3]]
     tmp = logics[question_number][3]
@@ -171,7 +321,7 @@ for question_number in range(4):
     buttons.append(create_button(655, 600, 150, 50, (0, 255, 0), "Option 3", (255, 0, 0), 24, action=2))
 
     question_answered = False
-    time_limit = 120000
+    time_limit = 60000
     elapsed_time = pygame.time.get_ticks() - start_time
     
     remaining_time = max(time_limit - elapsed_time, 0)
@@ -181,9 +331,10 @@ for question_number in range(4):
     while not question_answered and running:
         
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 running = False
-
+                
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 for button in buttons:
@@ -192,11 +343,11 @@ for question_number in range(4):
                         if check_answer(action, correct_option):
                             score += 1
                         question_answered = True
-
+                    
         WINDOW.fill((0, 0, 0))  
 
         elapsed_time = pygame.time.get_ticks() - start_time
-        time_limit = 120000
+        time_limit = 60000
         remaining_time = max(time_limit - elapsed_time, 0)
         WINDOW.fill((0, 0, 0))
         font = pygame.font.Font(None, 36)
@@ -213,7 +364,7 @@ for question_number in range(4):
         for i, matrix in enumerate(options):
             x = 150 + i * 250  
             y = 400  
-            display_matrix(matrix, x, y, square_size=40)  
+            display_matrix(matrix, x, y, square_size = 40)  
 
         # Display the buttons
         for button in buttons:
@@ -230,17 +381,29 @@ for question_number in range(4):
 
     buttons.clear()
 
-def display_iq_score(score, x, y, font_size=136, color=(255, 255, 255)):
+def display_iq_score(score, x, y, font_size=66, color=(255, 255, 255)):
+    
     font = pygame.font.Font(None, font_size)
-    text = "Your IQ is:  "
+
+    text = "Your estimated IQ is: ~ "
     if score == 0:
         text += "50"
     elif score == 1:
         text += "80"
     elif score == 2:
+        text += "90"
+    elif score == 3:
         text += "100"
+    elif score == 4:
+        text += "105"
+    elif score == 5:
+        text += "110"
+    elif score == 6:
+        text += "120"
+    elif score == 7:
+        text += "128"
     else:
-        text += "150"
+        text = "Your estimated IQ is over 135"
 
     text_surface = font.render(text, True, color)
     text_rect = text_surface.get_rect()
@@ -251,6 +414,7 @@ def display_iq_score(score, x, y, font_size=136, color=(255, 255, 255)):
 running_iq_screen = True
 
 while running_iq_screen:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running_iq_screen = False
@@ -261,5 +425,10 @@ while running_iq_screen:
     display_iq_score(score, 150, 250)
 
     pygame.display.update()
+
+
+if player_name is not None:
+    with open('player_scores.txt', 'a') as file:
+        file.write(f"Name: {player_name}, Score: {score}, IQ: {text}\n")
 
 pygame.quit()
